833d1fe5835015a2c544e7bac0013d12ac8ae761
Elliot DeNolf <denolfe@gmail.com>
2026-07-29 22:35:58 -0400

    feat(theme): soften inline code color to warm tan

--- src/app/styles/theme.ts
+++ src/app/styles/theme.ts
@@ -16,7 +16,7 @@ export const theme = {
   h1Bg: '#2472c8',
   h1Fg: '#000000',
   codespanBg: '#3c3c3c',
-  codespanFg: '#f48771',
+  codespanFg: '#ce9178',
   kbdBg: '#3a3a3a',
   link: '#2472c8',
   navLink: '#23d18b',
